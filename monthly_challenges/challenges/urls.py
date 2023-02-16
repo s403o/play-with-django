da48387d1436ca14cf9ae71f452b85b99a4d051b
s403o <eslam.adel.me@gmail.com>
2023-02-16 16:51:02 +0200

    add dynamic path with segment

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('jan', views.jan),
-  path('feb', views.feb),
-  path('mar', views.mar)
+  # path('jan', views.jan),
+  # path('feb', views.feb),
+  # path('mar', views.mar)
+  path("<month>", views.monthly_challenges)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def jan(request):
-  return HttpResponse("learn Python")
+# def jan(request):
+#   return HttpResponse("learn Python")
 
-def feb(request):
-  return HttpResponse("learn PostgreSQL")
+# def feb(request):
+#   return HttpResponse("learn PostgreSQL")
 
-def mar(request):
-  return HttpResponse("learn Django")
+# def mar(request):
+#   return HttpResponse("learn Django")
+
+def monthly_challenges(request, month):
+  challenge_text = None
+  if month == 'jan':
+    challenge_text = "learn Python"
+  elif month == 'feb':
+    challenge_text = "learn PostgreSQL"
+  elif month == 'mar':
+    challenge_text = "learn Django"
+  else:
+    return HttpResponseNotFound("month not supported yet")
+  return HttpResponse(challenge_text)
