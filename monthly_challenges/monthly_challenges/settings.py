45264c243c53867a6e5c11cb3e19cb36de599945
s403o <eslam.adel.me@gmail.com>
2023-02-17 22:51:41 +0200

    install the app to make django aware of it (better way)

--- monthly_challenges/monthly_challenges/settings.py
+++ monthly_challenges/monthly_challenges/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'challenges'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "challenges" / "templates"
+            # BASE_DIR / "challenges" / "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
