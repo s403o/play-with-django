480307ec2472791dbbe73bd93cdea904bc0966f4
s403o <eslam.adel.me@gmail.com>
2023-02-17 22:44:00 +0200

    add templates

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
+
+
 monthly_challenges = {
-  "jan": "learn Python",
-  "feb": "learn PostgreSQL",
-  "mar": "learn Django",
-  "apr": "learn Data structure",
-  "may": "learn Ruby",
-  "jun": "learn Rails",
-  "jul": "learn redis",
-  "aug": "learn kafka",
-  "sep": "learn git",
-  "oct": "learn javascript",
-  "nov": "learn system design",
-  "dec": "learn algorithms",
+    "jan": "learn Python",
+    "feb": "learn PostgreSQL",
+    "mar": "learn Django",
+    "apr": "learn Data structure",
+    "may": "learn Ruby",
+    "jun": "learn Rails",
+    "jul": "learn redis",
+    "aug": "learn kafka",
+    "sep": "learn git",
+    "oct": "learn javascript",
+    "nov": "learn system design",
+    "dec": "learn algorithms",
 }
 # Create your views here.
 
@@ -26,45 +29,49 @@ monthly_challenges = {
 # def mar(request):
 #   return HttpResponse("learn Django")
 
+
 def index(request):
-  list_items = ""
-  months = list(monthly_challenges.keys())
+    list_items = ""
+    months = list(monthly_challenges.keys())
 
-  for month in months:
-    capitalized_month = month.capitalize()
-    month_path = reverse("month-challenge", args=[month])
-    list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
 
-  # response_data = """
-  #   <ul>
-  #     <li><a href="/challenges/jan">Jan</a></li>
-  #   </ul>
-  # """
-  response_data = f"<ul>{list_items}</ul>"
-  return HttpResponse(response_data)
+    # response_data = """
+    #   <ul>
+    #     <li><a href="/challenges/jan">Jan</a></li>
+    #   </ul>
+    # """
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 
 def monthly_challenge(request, month):
-  try:
-    challenge_text = monthly_challenges[month]
-    response_data = f"<h1>{challenge_text}</h1>"
-    return HttpResponse(response_data)
-  except:
-    return HttpResponseNotFound("<h1>month not supported yet!</h1>")
-    # if month == 'jan':
-    #     challenge_text = "learn Python"
-    # elif month == 'feb':
-    #     challenge_text = "learn PostgreSQL"
-    # elif month == 'mar':
-    #     challenge_text = "learn Django"
-    # else:
+    try:
+        challenge_text = monthly_challenges[month]
+        # response_data = f"<h1>{challenge_text}</h1>"
+        respose_data = render_to_string("challenges/challenge.html")
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound("<h1>month not supported yet!</h1>")
+        # if month == 'jan':
+        #     challenge_text = "learn Python"
+        # elif month == 'feb':
+        #     challenge_text = "learn PostgreSQL"
+        # elif month == 'mar':
+        #     challenge_text = "learn Django"
+        # else:
         # return HttpResponseNotFound("month not supported yet")
-    # return HttpResponse(challenge_text)
+        # return HttpResponse(challenge_text)
 
 
 def monthly_challenge_by_num(request, month):
-  months = list(monthly_challenges.keys())
-  if month > len(months):
-    return HttpResponseNotFound("<h1>Invalid month!</h1>")
-  redirect_month = months[month - 1]
-  redirect_path = reverse("month-challenge", args=[redirect_month]) # challenge/jan
-  return HttpResponseRedirect(redirect_path)
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("<h1>Invalid month!</h1>")
+    redirect_month = months[month - 1]
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # challenge/jan
+    return HttpResponseRedirect(redirect_path)
