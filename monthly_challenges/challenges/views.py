9d519c457fefe35baef938086c8d9d1c5f5a768b
s403o <eslam.adel.me@gmail.com>
2023-02-17 20:27:21 +0200

    returning html at code

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -30,9 +30,10 @@ monthly_challenges = {
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
-    return HttpResponse(challenge_text)
+    response_data = f"<h1>{challenge_text}</h1>"
+    return HttpResponse(response_data)
   except:
-    return HttpResponseNotFound("month not supported yet!")
+    return HttpResponseNotFound("<h1>month not supported yet!</h1>")
     # if month == 'jan':
     #     challenge_text = "learn Python"
     # elif month == 'feb':
@@ -47,7 +48,7 @@ def monthly_challenge(request, month):
 def monthly_challenge_by_num(request, month):
   months = list(monthly_challenges.keys())
   if month > len(months):
-    return HttpResponseNotFound("Invalid month!")
+    return HttpResponseNotFound("<h1>Invalid month!</h1>")
   redirect_month = months[month - 1]
   redirect_path = reverse("month-challenge", args=[redirect_month]) # challenge/jan
   return HttpResponseRedirect(redirect_path)
