8ec11d39a903748e1b60e0bff643261830eb9c48
s403o <eslam.adel.me@gmail.com>
2023-02-19 22:17:35 +0200

    include feature

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
   # path('feb', views.feb),
   # path('mar', views.mar)
   # order is matter so check first for int then str
-  path("", views.index), # /challenges/
+  path("", views.index, name="index"), # /challenges/
   path("<int:month>", views.monthly_challenge_by_num),
   path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
