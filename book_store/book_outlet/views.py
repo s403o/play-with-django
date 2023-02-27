73111496cbc41504bd908bae94cff929d1e39e58
s403o <eslam.adel.me@gmail.com>
2023-02-28 01:38:23 +0200

    render book details

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -7,3 +7,12 @@ def index(request):
     return render(request, "book_outlet/index.html", {
         "books": books
     })
+
+def book_detail(request, id):
+    book = Book.objects.get(pk=id)
+    return render(request, "book_detail.html", {
+        "tittle": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
