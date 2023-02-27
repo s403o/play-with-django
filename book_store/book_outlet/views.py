0b3fab89f86573165ce984690335fed879cafedc
s403o <eslam.adel.me@gmail.com>
2023-02-28 01:49:18 +0200

    handle error with shortcu

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, g
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.http import Http404
 
@@ -10,10 +10,11 @@ def index(request):
     })
 
 def book_detail(request, id):
-    try:
-      book = Book.objects.get(pk=id)
-    except:
-       raise Http404()
+    # try:
+    #   book = Book.objects.get(pk=id)
+    # except:
+    #    raise Http404()
+    book = get_object_or_404(Book, pk=id)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
