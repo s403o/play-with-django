836764ba32127745e84cf6d62f8112787acb1c62
s403o <eslam.adel.me@gmail.com>
2023-02-28 01:44:57 +0200

    add views + urls

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -10,8 +10,8 @@ def index(request):
 
 def book_detail(request, id):
     book = Book.objects.get(pk=id)
-    return render(request, "book_detail.html", {
-        "tittle": book.title,
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
         "author": book.author,
         "rating": book.rating,
         "is_bestseller": book.is_bestselling
