96c3e7a551c25d29d982adce7d7ccf5452c3e569
s403o <eslam.adel.me@gmail.com>
2023-03-11 22:16:41 +0200

    makemigration

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -12,14 +12,14 @@ class Book(models.Model):
         validators=[MinLengthValidator(1), MaxLengthValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default=""n null=False)  # book name 1 => book-name-1
+    slug = models.SlugField(default="", null=False)  # book name 1 => book-name-1
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[self.id])
     
-    def save(self, *args, **kwargs):# we called super because we want ovveide the built in save
+    def save(self, *args, **kwargs): # we called super because we want ovveide the built in save
         self.slug = slugify(self.title)
-        super.save(**args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
