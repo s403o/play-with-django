afd60448b7fcb153e2d786fb9ed7b5598927465a
s403o <eslam.adel.me@gmail.com>
2023-03-12 22:55:25 +0200

    use order (for sorting)

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -5,7 +5,7 @@ from django.db.models import Avg # Max, Min ..etc
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('-rating') # - for descending order
     num_books = books.count()
     avg_rating = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
