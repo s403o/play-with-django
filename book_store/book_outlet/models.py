ff205fcc06c3ef3b7c1e23ba58a210820ec5a7b2
s403o <eslam.adel.me@gmail.com>
2023-03-20 13:20:30 +0200

    configure admin settings

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -8,19 +8,18 @@ from django.utils.text import slugify # to transform tittle
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(
-        validators=[MinLengthValidator(1), MaxLengthValidator(5)])
+    rating = models.IntegerField(null=False)
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True,
-                             null=False, editable=False, db_index=True)  # book name 1 => book-name-1
+                             null=False, db_index=True)  # book name 1 => book-name-1
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[self.slug])
     
-    def save(self, *args, **kwargs): # we called super because we want ovveide the built in save
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs): # we called super because we want ovveide the built in save
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
