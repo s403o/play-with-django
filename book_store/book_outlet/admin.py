b2d6c4945f5ee39675b8bde72b40d1049c0df8a8
s403o <eslam.adel.me@gmail.com>
2023-03-25 00:42:36 +0200

    manage relations in Admin

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,4 +8,6 @@ class BookAdmin(admin.ModelAdmin):
    list_filter = ("author", "rating")
    list_display = ("title", "author", "rating")
 
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
