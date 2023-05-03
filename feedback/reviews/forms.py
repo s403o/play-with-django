f3d15fcb3c91038b9cee51d50144b91b45f2b2e0
Eslam Adel <eslam.adel.me@gmail.com>
2023-05-03 14:12:46 +0300

    model form (saving data and some configs)

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,16 +1,43 @@
 from django import forms
+from .models import Review
 
+# class Reviewform(forms.Form):
+#     name = forms.CharField(label='Your name', max_length=50, error_messages= {
+#         'required': 'Please enter your name - must be not empty',
+#         'max_length': 'Your name is too long'
+#     })
+#     review_text = forms.CharField(label='Your feedback', widget=forms.Textarea, max_length=1000, error_messages= {
+#         'required': 'Please enter your feedback - must be not empty',
+#         'max_length': 'Your feedback is too long'
+#     })
+#     rating = forms.IntegerField(label='Your rating', min_value=1, max_value=5, error_messages= {
+#         'max_value': 'Please enter a rating between 1 and 5',
+#         'min_value': 'Please enter a rating between 1 and 5'
+#     })
 
-class Reviewform(forms.Form):
-    name = forms.CharField(label='Your name', max_length=50, error_messages= {
-        'required': 'Please enter your name - must be not empty',
-        'max_length': 'Your name is too long'
-    })
-    review_text = forms.CharField(label='Your feedback', widget=forms.Textarea, max_length=1000, error_messages= {
-        'required': 'Please enter your feedback - must be not empty',
-        'max_length': 'Your feedback is too long'
-    })
-    rating = forms.IntegerField(label='Your rating', min_value=1, max_value=5, error_messages= {
-        'max_value': 'Please enter a rating between 1 and 5',
-        'min_value': 'Please enter a rating between 1 and 5'
-    })
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            'name': 'Your name',
+            'review_text': 'Your feedback',
+            'rating': 'Your rating'
+        }
+        error_messages = {
+            'name': {
+                'required': 'Please enter your name - must be not empty',
+                'max_length': 'Your name is too long'
+            },
+            'review_text': {
+                'required': 'Please enter your feedback - must be not empty',
+                'max_length': 'Your feedback is too long'
+            },
+            'rating': {
+                'max_value': 'Please enter a rating between 1 and 5',
+                'min_value': 'Please enter a rating between 1 and 5'
+            }
+        }
+        widgets = {
+            'review_text': forms.Textarea(attrs={'cols': 40, 'rows': 5})
+        }
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import Reviewform
-from .models import Review
+# from .models import Review
 
 # Create your views here.
 
 
 def review(request):
     if request.method == "POST":
-        form = Reviewform(request.POST)
+        # existing_data = Review.objects.get(pk=1)
+        form = Reviewform(request.POST) # instance=existing_data (for update)
         if form.is_valid():
-            review = Review(
-                name=form.cleaned_data["name"],
-                review_text=form.cleaned_data["review_text"],
-                rating=form.cleaned_data["rating"],
-            )
-            review.save()
+            form.save()
+            # review = Review(
+            #     name=form.cleaned_data["name"],
+            #     review_text=form.cleaned_data["review_text"],
+            #     rating=form.cleaned_data["rating"],
+            # )
+            # review.save()
             # print(form.cleaned_data) # {'name': 'name'}
 
             return HttpResponseRedirect("/thank_you")
