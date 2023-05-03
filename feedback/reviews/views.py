10283dc0a5b32a901eb89d91fb18fc4b1165460e
Eslam Adel <eslam.adel.me@gmail.com>
2023-05-03 13:09:39 +0300

    save to DB

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import Reviewform
+from .models import Review
 
 # Create your views here.
 
+
 def review(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = Reviewform(request.POST)
         if form.is_valid():
-            print(form.cleaned_data) # {'name': 'name'}
-            return HttpResponseRedirect('/thank_you')
+            review = Review(
+                name=form.cleaned_data["name"],
+                review_text=form.cleaned_data["review_text"],
+                rating=form.cleaned_data["rating"],
+            )
+            review.save()
+            # print(form.cleaned_data) # {'name': 'name'}
+
+            return HttpResponseRedirect("/thank_you")
     else:
         form = Reviewform()
 
-    return render(request, 'reviews/review.html', {
-        'form': form
-    })
+    return render(request, "reviews/review.html", {"form": form})
+
 
 def thank_you(request):
-    return render(request, 'reviews/thank_you.html') 
+    return render(request, "reviews/thank_you.html")
